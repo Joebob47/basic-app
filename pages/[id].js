b444f05a8cc2a24b0b6acbd78c80fbf80961c929
Joe Linnemeier <joe.linnemeier@gmail.com>
2022-11-15 13:20:33 -0500

    updated data.js, [id].js, fixed html tag display

--- pages/[id].js
+++ pages/[id].js
@@ -14,9 +14,9 @@ export async function getStaticProps({ params }) {
 }
 
 //create an instance of the getStaticPaths() function to report to Next all possible dynamic URLs.
-//this function just returns an array of ids in a particular way with those params in it
+///this function just returns an array of ids in a particular way with those params in it
 export async function getStaticPaths() {
-  const paths = getAllIds();
+  const paths =  await getAllIds();
 
   console.log(paths);
   return {
@@ -29,26 +29,19 @@ export async function getStaticPaths() {
 //itemData here will be the entire chunk of info for the specific id
 //THIS FORMAT IS CONFUSING TO ME
 export default function Entry({ itemData }) {
+  let noTagString = itemData[0].post_content;
+  noTagString=noTagString.replace('<!-- wp:paragraph -->','');
+  noTagString=noTagString.replace('<p>','');
+  noTagString=noTagString.replace('</p>','');
+  noTagString=noTagString.replace('<!-- /wp:paragraph -->','');
   return (
     <Layout>
-      <article className="card col-6 mx-auto">
-        <h5 class="text-center"> Employee </h5>
+      <article className="card col-6 mx-auto text-center">
+        <h1> {itemData[0].post_title} </h1>
         <div className="card-body">
-          <h5 className="card-title">{itemData[0].name}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">{itemData[0].phone}</h6>
-          <p className="card-text">{itemData[0].birthdate}</p>
+          <p className="card-text">{noTagString}</p>
+          <h6 className="card-subtitle mb-2 text-muted">Last modified: {itemData[0].post_modified}</h6>
           <a href={'mailto:' + itemData[0].email} className="card-link">{itemData[0].email}</a>
-        </div>
-        
-      </article>
-
-            <article className="card col-6 mx-auto">
-              <h5 class="text-center">Emergency Contact</h5>
-        <div className="card-body">
-          <h5 className="card-title">{itemData[1].name}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">{itemData[1].phone}</h6>
-          <p className="card-text">{itemData[1].birthdate}</p>
-          <a href={'mailto:' + itemData[1].email} className="card-link">{itemData[1].email}</a>
         </div>
         
       </article>
